5c8263570cae6499d3ea0b09d1c981aebd2fd61f
“CJ” <1592105+cjluzroque@users.noreply.github.com>
2025-01-04 15:30:45 -0500

    Create function to add Book objects

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(name, author, pages, read) {
     this.name = name; 
     this.author = author; 
@@ -16,16 +18,20 @@ function Book(name, author, pages, read) {
   Book.prototype.testProto = function() {
     console.log("Proto test! " + this.name);
  };
+
+function addBookToLibrary() {
+    // do stuff here
+}
+
  
-  
-  const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
-  console.log(Object.getPrototypeOf(book1) === Book.prototype); // returns true
-  book1.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, already read"
-  book1.testProto();
-  const book2 = new Book('Test Book', 'Big Boi', 321, false);
-  console.log(Object.getPrototypeOf(book2) === Book.prototype);
-  book2.info();
-  book2.testProto();
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+console.log(Object.getPrototypeOf(book1) === Book.prototype); // returns true
+book1.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, already read"
+book1.testProto();
+const book2 = new Book('Test Book', 'Big Boi', 321, false);
+console.log(Object.getPrototypeOf(book2) === Book.prototype);
+book2.info();
+book2.testProto();
   
 console.log(Object.getPrototypeOf(Book.prototype) === Object.prototype); // true
 console.log(book1.valueOf()); // Output: Book {name: 'The Hobbit', author: 'J.R.R. Tolkien', pages: 295, read: true, info: ƒ}
