a695380e4990972349b6ddf134748773b5660512
“CJ” <1592105+cjluzroque@users.noreply.github.com>
2025-01-12 13:07:45 -0500

    Add remove icon that will remove the card

--- script.js
+++ script.js
@@ -46,25 +46,48 @@ function addBookToLibrary() {
     const layoutarea = document.querySelector('.layout');
     const newbook = document.createElement("div");
     newbook.classList.add('card');
+
+    // Add Title to the book card
     const booktitle = document.createElement("p");
     booktitle.classList.add('title');
     booktitle.textContent = 'TITLE OF BOOK'; // TESTING TITLE
     newbook.appendChild(booktitle);
+
+    // Add Author to the book card
     const bookauthor = document.createElement("p");
     bookauthor.classList.add('description');
     bookauthor.textContent = 'AUTHOR OF BOOK'; // TESTING AUTHOR
     newbook.appendChild(bookauthor);
+
+    // Add Page Count to the book card
     const bookpages = document.createElement("p");
     bookpages.classList.add('description');
     bookpages.textContent = 'PAGES OF BOOK'; // TESTING PAGES
     newbook.appendChild(bookpages);
-    layoutarea.appendChild(newbook);
 
-    // Create card from Book 
-
-    // Add to page 
+    // Add remove icon to the book card 
+    const bookaction = document.createElement("div");
+    bookaction.classList.add('actions');
+    const removeIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const removePath = document.createElementNS(
+      'http://www.w3.org/2000/svg',
+      'path'
+    );
+    removeIcon.setAttribute('viewBox', '0 0 24 24');
+    removePath.setAttribute(
+      'd',
+      'M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z'
+    );
+    removeIcon.appendChild(removePath);
+    bookaction.appendChild(removeIcon);
+    newbook.appendChild(bookaction);
+    layoutarea.appendChild(newbook); // Add card to the layout 
 
     // Remove function for card 
+    bookaction.addEventListener("click", function () {
+      console.log('Remove button pressed');
+      newbook.remove();
+    });
 
 }
 
