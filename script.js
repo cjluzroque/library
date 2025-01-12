25f4d05ed9269d532449aff31eec4c44cad8aee1
“CJ” <1592105+cjluzroque@users.noreply.github.com>
2025-01-12 13:50:36 -0500

    Create basic form and insert values into book card

--- script.js
+++ script.js
@@ -17,10 +17,6 @@ function Book(name, author, pages, read) {
     };
   }
 
-  Book.prototype.testProto = function() {
-    console.log("Proto test! " + this.name);
- };
-
 
 // Create new book cards 
 const createBook = document.querySelector('.createBook');
@@ -31,77 +27,94 @@ createBook.addEventListener("click", function () {
 
 function addBookToLibrary() {
     // Create dialogue box with form element 
+    const layout = document.querySelector('.container');
+    const bookdialogue = document.createElement('div');
+    bookdialogue.classList.add('bookdialogue');
+    const bookform = document.createElement('form');
+    bookform.classList.add('bookform');
+    const titleinput = document.createElement('input');
+    titleinput.classList.add('titleinput');
+    bookform.appendChild(titleinput);
+    titleinput.value = 'TITLE'// Test title input
+    const authorinput = document.createElement('input');
+    authorinput.classList.add('authorinput');
+    bookform.appendChild(authorinput);
+    authorinput.value = 'AUTHOR'// Test title input
+    const pageinput = document.createElement('input');
+    pageinput.classList.add('pageinput');
+    bookform.appendChild(pageinput);
+    pageinput.value = 'PAGES'// Test title input
+    const readinput = document.createElement('input');
+    readinput.classList.add('readinput');
+    bookform.appendChild(readinput);
+    readinput.value = 'READ'// Test title input
+    const submitbook = document.createElement('button');
+    submitbook.classList.add('submitbook');
+    bookform.appendChild(submitbook);
+    bookdialogue.appendChild(bookform);
+    layout.appendChild(bookdialogue);
 
-    // Input Name
+    // Create Book Object and remove dialogue
+    bookform.addEventListener('submit', (event) => {
+      // Prevent the default form submission behavior
+      event.preventDefault(); 
 
-    // Input Author 
+      // Create Object
+      console.log('Creating Object');
+      const book = new Book(titleinput.value, authorinput.value, pageinput.value, readinput.value);
+      console.log(book);
 
-    // Input Pages
+      // Remove dialogue box
+      bookdialogue.remove();
 
-    // Input Read state 
+      // Create Book from form input 
+      const layoutarea = document.querySelector('.layout');
+      const newbook = document.createElement("div");
+      newbook.classList.add('card');
 
-    // Remove dialogue box when done
+      // Add Title to the book card
+      const booktitle = document.createElement("p");
+      booktitle.classList.add('title');
+      booktitle.textContent = book.name; 
+      newbook.appendChild(booktitle);
 
-    // Create Book from form input 
-    const layoutarea = document.querySelector('.layout');
-    const newbook = document.createElement("div");
-    newbook.classList.add('card');
+      // Add Author to the book card
+      const bookauthor = document.createElement("p");
+      bookauthor.classList.add('description');
+      bookauthor.textContent = book.author;
+      newbook.appendChild(bookauthor);
 
-    // Add Title to the book card
-    const booktitle = document.createElement("p");
-    booktitle.classList.add('title');
-    booktitle.textContent = 'TITLE OF BOOK'; // TESTING TITLE
-    newbook.appendChild(booktitle);
+      // Add Page Count to the book card
+      const bookpages = document.createElement("p");
+      bookpages.classList.add('description');
+      bookpages.textContent = book.pages;
+      newbook.appendChild(bookpages);
 
-    // Add Author to the book card
-    const bookauthor = document.createElement("p");
-    bookauthor.classList.add('description');
-    bookauthor.textContent = 'AUTHOR OF BOOK'; // TESTING AUTHOR
-    newbook.appendChild(bookauthor);
+      // Add remove icon to the book card 
+      const bookaction = document.createElement("div");
+      bookaction.classList.add('actions');
+      const removeIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+      const removePath = document.createElementNS(
+        'http://www.w3.org/2000/svg',
+        'path'
+      );
+      removeIcon.setAttribute('viewBox', '0 0 24 24');
+      removePath.setAttribute(
+        'd',
+        'M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z'
+      );
+      removeIcon.appendChild(removePath);
+      bookaction.appendChild(removeIcon);
+      newbook.appendChild(bookaction);
+      layoutarea.appendChild(newbook); // Add card to the layout 
 
-    // Add Page Count to the book card
-    const bookpages = document.createElement("p");
-    bookpages.classList.add('description');
-    bookpages.textContent = 'PAGES OF BOOK'; // TESTING PAGES
-    newbook.appendChild(bookpages);
-
-    // Add remove icon to the book card 
-    const bookaction = document.createElement("div");
-    bookaction.classList.add('actions');
-    const removeIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    const removePath = document.createElementNS(
-      'http://www.w3.org/2000/svg',
-      'path'
-    );
-    removeIcon.setAttribute('viewBox', '0 0 24 24');
-    removePath.setAttribute(
-      'd',
-      'M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z'
-    );
-    removeIcon.appendChild(removePath);
-    bookaction.appendChild(removeIcon);
-    newbook.appendChild(bookaction);
-    layoutarea.appendChild(newbook); // Add card to the layout 
-
-    // Remove function for card 
-    bookaction.addEventListener("click", function () {
-      console.log('Remove button pressed');
-      newbook.remove();
+      // Remove function for card 
+      bookaction.addEventListener("click", function () {
+        console.log('Remove button pressed');
+        newbook.remove();
+      });
     });
 
-}
+    
 
-const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
-console.log(Object.getPrototypeOf(book1) === Book.prototype); // returns true
-book1.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, already read"
-book1.testProto();
-const book2 = new Book('Test Book', 'Big Boi', 321, false);
-console.log(Object.getPrototypeOf(book2) === Book.prototype);
-book2.info();
-book2.testProto();
-  
-console.log(Object.getPrototypeOf(Book.prototype) === Object.prototype); // true
-console.log(book1.valueOf()); // Output: Book {name: 'The Hobbit', author: 'J.R.R. Tolkien', pages: 295, read: true, info: ƒ}
-console.log(book1.hasOwnProperty('valueOf')); // false
-console.log(Object.prototype.hasOwnProperty('valueOf')); // true
-Object.prototype.hasOwnProperty('hasOwnProperty'); // true
+}
